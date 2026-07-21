2e7c3b7bbde16498455632efacd06c7cfa5a60ed
TangoMermaid <tango.mermaid.tm@gmail.com>
2026-07-21 12:13:52 +0300

    Add files via upload

---
+++ webpage/checkout.js
@@ -0,0 +1,66 @@
+let products = [];
+
+const logo_file = "Logo board_Colour_PNG.png";
+
+const compliments = [
+    "✨ Treasure found!",
+    "💙 Great choice!",
+    "🌊 I love your style!",
+    "✨ What a lovely find!",
+    "💃 This will look wonderful on you!",
+    "🌸 Get ready to collect compliments!",
+    "🧜 Another beautiful Tango Mermaid piece!"
+];
+
+fetch("../public_data/products.json")
+    .then(response => response.json())
+    .then(data => {
+        products = data;
+    });
+
+document.getElementById("header").innerHTML = `
+    <img src="${logo_file}" alt="Tango Mermaid" id="logo">
+
+`;    
+
+function findItem() {
+
+    let code = document.getElementById("itemCode").value.toUpperCase();
+
+    let item = products.find(product => product["Item ID"] === code);
+
+    let result = document.getElementById("result");
+
+    if (item) {
+
+        // Select a random encouragement
+        const compliment =
+            compliments[Math.floor(Math.random() * compliments.length)];
+
+        result.innerHTML = `
+            <img src="../item_photos/${item["Item ID"]}.jpg"
+                alt="${item["Item ID"]}"
+                id="productPhoto">
+
+            <h2 class="compliment">${compliment}</h2>    
+
+            <p><b>Item ID: ${item["Item ID"]}</p>
+
+            <p><b>Product:</b> ${item["Category"]}</p>
+
+            <p><b>Description:</b> ${item["Sub-category"]}</p>
+
+            <p><b>Price:</b> €${item["ESTIMATED selling price, EURO"]}</p>
+
+            <br>
+
+            <button disabled>🛒 Add to basket</button>
+
+            <button disabled>Continue shopping</button>
+        `;
+
+    } else {
+
+        result.innerHTML = "Item not found";
+    }
+}
