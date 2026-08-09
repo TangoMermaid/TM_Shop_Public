43dad2508dd1f9be1ae660a82441ea59cd85ed13
TangoMermaid <tango.mermaid.tm@gmail.com>
2026-08-09 11:23:25 +0300

    Fetch GitHub repository data in Netlify function

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -1,6 +1,22 @@
 exports.handler = async () => {
+
+    const response = await fetch(
+        "https://api.github.com/repos/TangoMermaid/TM_Shop_Secure",
+        {
+            headers: {
+                "Authorization": `Bearer ${process.env.GITHUB_TOKEN}`,
+                "Accept": "application/vnd.github+json"
+            }
+        }
+    );
+
+    const data = await response.json();
+
     return {
-        statusCode: 200,
-        body: "Netlify bridge works!"
+        statusCode: response.status,
+        body: JSON.stringify({
+            repository: data.name,
+            private: data.private
+        })
     };
 };
